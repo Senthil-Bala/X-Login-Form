feb6d518ca0bfae179b86ae5052db7e91c99e501
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-21 18:20:10 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [isLogin, setisLogin] = useState(false);
+  const [errorMessage, seterrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    if (username === "user" && password === "password") {
+      e.preventDefault();
+      setisLogin(true);
+      seterrorMessage(null);
+      
+    } else {
+      seterrorMessage("Invalid username or password");
+      setisLogin(false);
+    }
+  };
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit}>
+        <h1>Login Page</h1>
+        <label>Username:</label>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => {
+            setusername(e.target.value);
+          }}
+          required
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          type="text"
+          placeholder="password"
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+          required
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {isLogin ? <h2>Welcome, user!</h2> : null}
+      {errorMessage ? <h2>{errorMessage}</h2> : null}
     </div>
   );
 }
