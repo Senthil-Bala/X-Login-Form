459c7ed5d4fbd242f472fcac3a7b27bfda49cac3
Senthil-Bala <senthilkumarbala100@gmail.com>
2023-12-21 19:50:38 +0530

    error messsage added

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ function App() {
   const [errorMessage, seterrorMessage] = useState("");
 
   const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    
     if (username === "user" && password === "password") {
-      e.preventDefault();
       setisLogin(true);
       seterrorMessage(null);
-      
     } else {
       seterrorMessage("Invalid username or password");
       setisLogin(false);
     }
   };
+  
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <h1>Login Page</h1>
+        {errorMessage ? <h2>{errorMessage}</h2> : null}
         <label>Username:</label>
         <input
           type="text"
@@ -45,7 +47,7 @@ function App() {
         <button type="submit">Submit</button>
       </form>
       {isLogin ? <h2>Welcome, user!</h2> : null}
-      {errorMessage ? <h2>{errorMessage}</h2> : null}
+      
     </div>
   );
 }
